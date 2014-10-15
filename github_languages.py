208369ccbc1b42456c894276792f115292e4b01e
caduc <remaier@gmail.com>
2014-10-15 19:14:10 -0400

    add passwords and outputs of API

--- github_languages.py
+++ github_languages.py
@@ -5,11 +5,12 @@ import operator
 from collections import defaultdict
 
 import requests
+from secret import USERNAME, PASSWORD
 
 def git_repositories(user):
 	""" Retrieve a list of a users repositories """
 	url = "https://api.github.com/users/{user}/repos".format(user=user)
-	response = requests.get(url)
+	response = requests.get(url, auth=(USERNAME,PASSWORD))
 	return response.json()
 
 def main():
