c463c55c69f6c3625405061b647a1c307ce00aec
caduc <remaier@gmail.com>
2014-10-15 20:40:19 -0400

    discuss

--- github_languages.py
+++ github_languages.py
@@ -7,16 +7,33 @@ from collections import defaultdict
 import requests
 from secret import USERNAME, PASSWORD
 
-def git_repositories(user):
+def get_repositories(user):
 	""" Retrieve a list of a users repositories """
 	url = "https://api.github.com/users/{user}/repos".format(user=user)
+	#  https://api.github.com/users/Caduc/repos
 	response = requests.get(url, auth=(USERNAME,PASSWORD))
+	#module called python gnu pgp - check it out.  another is hashlib. getpass is a module and function
 	return response.json()
+
+def get_language_dictionaries(repositories):
+	#return list of dictionaries
+	language_dictionaries = []
+	for repository in repositories:
+		url = "https://api.github.com/repos/{owner}/{repo}/languages"
+		owner = repository["owner"]["login"]
+		repo = repository["name"]
+		url = url.format(owner,repo)
+		response = requests.get(url, auth=(USERNAME,PASSWORD))
+		language_dictionaries.append(response.json())
+	return language_dictionaries
+	
 
 def main():
 	""" Main function """
-	repositories = git_repositories(sys.argv[1])
+	repositories = get_repositories(sys.argv[1])
+	language = get_language_dictionaries(repositories)
 	print repositories
+	print language
 
 
 if __name__ == '__main__':
